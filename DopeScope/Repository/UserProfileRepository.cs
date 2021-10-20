1180c05577f6a3dce11a5d8915a8a1fe80cc5e33
Sam Carbine <scarbine@me.com>
2021-10-20 15:59:04 -0500

    fixed login issue

--- DopeScope/Repository/UserProfileRepository.cs
+++ DopeScope/Repository/UserProfileRepository.cs
@@ -20,10 +20,13 @@ namespace DopeScope.Repository
                 using (var cmd = conn.CreateCommand())
                 {
                     cmd.CommandText = @"
-                        SELECT up.Id, Up.FirebaseUserId, up.FirstName AS UserProfileFirstName, up.LastName AS UserProfileLastName up.Email, up.UserTypeId,
-                         WHERE FirebaseUserId = @FirebaseuserId";
+                        SELECT u.Id, u.FirebaseId, u.FirstName AS UserProfileFirstName, u.LastName AS UserProfileLastName, u.Email
+                           
+                          FROM [User] u
+                          
+                         WHERE FirebaseId = @FirebaseUserId";
 
-                    DbUtils.AddParameter(cmd, "@FirebaseUserId", firebaseUserId);
+                    DbUtils.AddParameter(cmd, "@FirebaseuserId", firebaseUserId);
 
                     UserProfile userProfile = null;
 
@@ -33,8 +36,8 @@ namespace DopeScope.Repository
                         userProfile = new UserProfile()
                         {
                             Id = DbUtils.GetInt(reader, "Id"),
-                            FirebaseUserId = DbUtils.GetString(reader, "FirebaseUserId"),
-                            FirstName = DbUtils.GetString(reader, "UserProfileName"),
+                            FirebaseUserId = DbUtils.GetString(reader, "FirebaseId"),
+                            FirstName = DbUtils.GetString(reader, "UserProfileFirstName"),
                             LastName = DbUtils.GetString(reader, "UserProfileLastName"),
                             Email = DbUtils.GetString(reader, "Email"),
                            
@@ -55,9 +58,9 @@ namespace DopeScope.Repository
                 conn.Open();
                 using (var cmd = conn.CreateCommand())
                 {
-                    cmd.CommandText = @"INSERT INTO UserProfile (FirebaseUserId, FirstName, LastName, Email, UserTypeId)
+                    cmd.CommandText = @"INSERT INTO User (FirebaseId, FirstName, LastName, Email)
                                         OUTPUT INSERTED.ID
-                                        VALUES (@FirebaseUserId, @FirstName, @LastName @Email, @UserTypeId)";
+                                        VALUES (@FirebaseUserId, @FirstName, @LastName @Email)";
                     DbUtils.AddParameter(cmd, "@FirebaseUserId", userProfile.FirebaseUserId);
                     DbUtils.AddParameter(cmd, "@FirstName", userProfile.FirstName);
                     DbUtils.AddParameter(cmd, "@LastName", userProfile.LastName);
