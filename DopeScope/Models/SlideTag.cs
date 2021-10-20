5697cb23d72a8abf5115e00417815e11409b22d1
Sam Carbine <scarbine@me.com>
2021-10-20 08:59:39 -0500

    added models to the api

---
+++ DopeScope/Models/SlideTag.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DopeScope.Models
+{
+    public class SlideTag
+    {
+
+        public int Id { get; set; }
+
+        public int SlideId { get; set; }
+
+        public Slide Slide { get; set; }
+
+        public int TagId { get; set; }
+
+        public Tag Tag { get; set; }
+    }
+}
--- DopeScope/Repository/UserRepository.cs
+++ DopeScope/Repository/UserRepository.cs
@@ -7,5 +7,42 @@ namespace DopeScope.Repository
 {
     public class UserRepository
     {
+        // GET slide  microscope and user data
+        //  @"SELECT * FROM Slide s 
+        //  LEFT JOIN Microscope m ON m.id = s.MicroscopeId 
+        //  LEFT JOIN [User] u ON u.id = m.UserId
+        //  LEFT JOIN Note n ON n.slideId = s.id"
+
+        // GET slides List of Tags by slide Id
+        //  @"SELECT * FROM SlideTag st
+        //  LEFT JOIN Slide s ON s.id = st.SlideId
+        //  LEFT JOIN Tag u ON u.id = st.TagId
+        //  WHERE s.id = @id"
+
+
+        // GET list of slides of a mircoscope
+        //  @"SELECT* FROM Slide s
+        //  JOIN Microscope m ON m.id = s.MicroscopeId
+        //  WHERE s.MicroscopeId = @id"
+
+        // GET list of slides of a user
+        // @"SELECT * FROM Slide s
+        //    JOIN Microscope m ON m.id = s.MicroscopeId
+        //    JOIN[User] u ON m.UserId = u.id
+        //    WHERE u.id = @id"
+
+        // GET list of slides by tag
+        //@"SELECT * From SlideTag st
+        // JOIN Slide s on s.Id = st.SlideId
+        // JOIN Tag t on t.id = st.TagId
+        // JOIN Microscope m ON m.Id = s.MicroscopeId
+        // JOIN[User] u ON u.Id = m.UserId
+        // WHERE t.id = @id"
+
+        // GET List of notes for a slide
+        // @"SELECT* FROM Note n
+        //  JOIN[User] u ON u.Id = n.UserId
+        //  JOIN Slide s ON s.Id = n.SlideId
+        //  WHERE s.Id = 1"
     }
 }
