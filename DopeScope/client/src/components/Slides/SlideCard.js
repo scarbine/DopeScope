8fcfe779a4da505c026e7537c5293dcf734b29ce
Sam Carbine <scarbine@me.com>
2021-10-21 12:17:26 -0500

    addimg width to slide card

--- DopeScope/client/src/components/Slides/SlideCard.js
+++ DopeScope/client/src/components/Slides/SlideCard.js
@@ -31,8 +31,8 @@ export const SlideCard = ({ slide , updateList }) => {
   return (
     <>
       <div>
-        <Card>
-          {/* <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" /> */}
+        <Card className="slide-card">
+          <CardImg top width="100%" src={slide.imageUrl} alt="Card image cap" />
           <CardBody>
             <CardTitle tag="h5">{slide.name}</CardTitle>
             <CardSubtitle tag="h6" className="mb-2 text-muted">
@@ -42,7 +42,7 @@ export const SlideCard = ({ slide , updateList }) => {
             </CardSubtitle>
             <CardText>{slide.description}</CardText>
             <div className="slide-buttons">
-            <Button className="slide-btn" onClick={handleDetails}>Edit Details</Button>
+            <Button className="slide-btn" onClick={handleDetails}>Details</Button>
             <Button className="slide-btn" onClick={handleEdit}>Edit Slide</Button>
             <Button className="slide-btn" onClick={handleDelete}>Delete Slide</Button>
             </div>
